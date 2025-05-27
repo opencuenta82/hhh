7c28a08353a9147bb7bd854c026be300116b7cc3
opencuenta82 <opencuenta82@gmail.com>
2025-05-27 06:58:32 -0500

    cami 3

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Logger
+// Loggggggggggggggggggger
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
